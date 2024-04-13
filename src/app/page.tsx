69f782d2e1cc964d5fd33c996298fb0cde76e815
Dário Silva <dariosilva13222@gmail.com>
2024-04-13 15:28:08 +0100

    style: changes the background color
    raw: creates two containers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello, World !</h1>
+    <main className="bg-gray-950 text-slate-50 flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex flex-row justify-between">
+          <div className="flex flex-col items-start gap-12">1</div>
+          <div className="flex flex-col items-start gap-12">2</div>
+      </div>
     </main>
   );
 }

f190ff2b6be0deb08b965267438491f51e9b1f3f
Dário Silva <dariosilva13222@gmail.com>
2024-04-14 11:19:13 +0100

    style(feat): creates a div where the qrcode will be rendered
    raw: creates a text description for the qrcode box

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,25 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="text-slate-50 flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex flex-row justify-between">
-          <div className="flex flex-col items-start gap-12">1</div>
-          <div className="flex flex-col items-start gap-12">2</div>
+    <main className="text-slate-50 flex min-h-screen flex-col items-start justify-between mx-7 my-10">
+      <div className="flex flex-row justify-between my-9">
+
+          <div className="flex flex-col items-start gap-2">
+            <p className="text-slate-100 font-semibold flex items-start">your generated qrcode
+            </p>
+
+            <div className="flex flex-col gap-8 bg-slate-800 w-80 h-80 rounded-md">
+
+            </div>
+
+            <div className="flex flex-row justify-between gap-5 items-start">
+
+            </div>
+
+          </div>
+          <div className="flex flex-col items-start gap-12">
+
+          </div>
       </div>
     </main>
   );

8d815cbc6e0bdc966a65c973675bc96ca0764341
Dário Silva <dariosilva13222@gmail.com>
2024-04-14 11:54:56 +0100

    style: changes navbar color to white

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,13 +18,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <nav className='flex flex-row justify-between items-center mx-7 my-7 bg-slate-800 rounded h-20 p-5'>
+        <nav className='flex flex-row justify-between items-center mx-7 my-7 bg-slate-50 rounded border-2 border-slate-200 h-20 p-5'>
             <div className='flex justify-between'>
                 <Image
                     alt='quicktranslator-logo'
                     src={''}
                 />
-                <h1 className='text-white font-bold'>QuickTranslator</h1>
+                <h1 className='text-slate-900 font-bold'>QuickTranslator</h1>
             </div>
             <div className='flex flex-row items-center justify-center'>
                 <Image 

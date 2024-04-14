c31768f93f9ad9435a83599d1c2f9759f7efdcd2
Dário Silva <dariosilva13222@gmail.com>
2024-04-14 15:06:12 +0100

    feat: adds a dropdown to render all languages
    raw: adds a textarea for the user's text
    
    style: changes label and dropdown texts colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-import Link from "next/link";
+'use client'
 
-export default function Home() {
+import { Button } from '@/components/ui/button'
+
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuRadioGroup, DropdownMenuRadioItem } from '@/components/ui/dropdown-menu'
+import { Label } from '@/components/ui/label'
+import { Textarea } from '@/components/ui/textarea'
+
+import Link from 'next/link'
+import * as React from 'react'
+
+export default function Home (): React.JSX.Element {
+  const [language, setLanguage] = React.useState('english')
+
   return (
     <main className="text-slate-50 flex min-h-screen flex-col items-start justify-between mx-7 my-10">
-      <div className="flex flex-row justify-between my-9">
+
+      <div className="flex flex-row justify-between mt-9">
 
           <div className="flex flex-col items-start gap-2">
             <p className="text-slate-900 font-semibold flex items-start">your generated qrcode
@@ -19,7 +29,7 @@ export default function Home() {
               <Button asChild className="h-15">
                 <Link href={'download'}>Download</Link>
               </Button>
-              <Button 
+              <Button
                 className="h-15"
               >
                   Share
@@ -27,10 +37,48 @@ export default function Home() {
             </div>
 
           </div>
-          <div className="flex flex-col items-start gap-12">
+
+          <div className="flex flex-col items-end gap-2">
+
+            <div className="flex flex-col items-center">
+                <DropdownMenu>
+
+                  <DropdownMenuTrigger asChild>
+                    <Button variant="outline">select your language</Button>
+                  </DropdownMenuTrigger>
+
+                  <DropdownMenuContent className="w-56">
+                    <DropdownMenuLabel>Languages</DropdownMenuLabel>
+
+                  <DropdownMenuSeparator />
+
+                    <DropdownMenuRadioGroup value={language} onValueChange={setLanguage}>
+                      <DropdownMenuRadioItem value="portuguese">Portuguese</DropdownMenuRadioItem>
+
+                      <DropdownMenuRadioItem value="French">French</DropdownMenuRadioItem>
+
+                      <DropdownMenuRadioItem value="German">German</DropdownMenuRadioItem>
+
+                      <DropdownMenuRadioItem value="Japanese">Japanese</DropdownMenuRadioItem>
+
+                      <DropdownMenuRadioItem value="Korean">Korean</DropdownMenuRadioItem>
+
+                      <DropdownMenuRadioItem value="Italian">Italian</DropdownMenuRadioItem>
+
+                    </DropdownMenuRadioGroup>
+
+                  </DropdownMenuContent>
+                </DropdownMenu>
+            </div>
+
+            <div className="flex flex-col">
+              <Label htmlFor="text" className='text-slate-900'>Your text</Label>
+              <Textarea placeholder="Type your text here." id="text" className='text-slate-900 p-20'/>
+            </div>
 
           </div>
       </div>
+
     </main>
-  );
+  )
 }

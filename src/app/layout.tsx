fe9410b3ead92837c968f89b2cee335ed6495444
Dário Silva <dariosilva13222@gmail.com>
2024-04-14 11:05:34 +0100

    feat: adds a navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import navbar from '../components/navbar';
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +18,23 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <nav className='flex flex-row justify-between items-center mx-7 my-7 bg-slate-800 rounded h-20 p-5'>
+            <div className='flex justify-between'>
+                <Image
+                    alt='quicktranslator-logo'
+                    src={''}
+                />
+                <h1 className='text-white font-bold'>QuickTranslator</h1>
+            </div>
+            <div className='flex flex-row items-center justify-center'>
+                <Image 
+                    alt='github-logo'
+                    src={''}
+                />
+            </div>
+        </nav>        
         {children}
-        </body>
+      </body>
     </html>
   );
 }

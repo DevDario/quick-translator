c9d5a7ab0fee43f401efb5e5512d14fa8d8d94e2
Dário Silva <dariosilva13222@gmail.com>
2024-04-14 11:04:57 +0100

    style(raw): removes tailwind background color definition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="bg-gray-950 text-slate-50 flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="text-slate-50 flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex flex-row justify-between">
           <div className="flex flex-col items-start gap-12">1</div>
           <div className="flex flex-col items-start gap-12">2</div>
